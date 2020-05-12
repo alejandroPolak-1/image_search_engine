2a276671517bd301796bc6213ac3646c9859afa3
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-12 10:54:07 -0300

    [Form] Validate [Error.js]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
 import Form from './components/Form'
-
 
 function App() {
   return (
     <div className="conteiner">
       <div className="jumbotron">
-        <p className="lead text-center">
-          Image Search Engine
-        </p>
+        <p className="lead text-center">Image Search Engine</p>
         <Form />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Error from './Error.js'
 
 const Form = () => {
+
+//put in the state what is put in the input
+    const [term, setTerm]= useState('')
+    const [error, setError] = useState(false)
+
+//searchImage
+const handleSubmit = e => {
+    e.preventDefault()
+
+    //Validate
+    if(term.trim()=== '') {
+        setError(true)
+        return
+    }
+    setError(false)
+  
+    //send search term to main component
+
+}
+
   return (
-    <form>
+    <form
+    onSubmit={handleSubmit}
+    >
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Search an image"
+            onChange={e => setTerm(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +43,8 @@ const Form = () => {
           />
         </div>
       </div>
+
+      {error ? <Error message="add a search term" /> :null}
     </form>
   )
 }
